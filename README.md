0169f18cbdd80333fe281314a2fb12f96803b2a4
sajithv <sajithv@hsenidmobile.com>
2019-10-07 17:04:52 +0530

    Fix: shema added & readme Updated

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # spring-oauth-server-demo
 This is Oauth Server Demo Application  fro Test Spring Oauth Server [ JDBC Token Store]
+
+
+### Get Oauth Token
+
+curl -X POST \
+  'http://localhost:8081/oauth/token?grant_type=password&username=admin&password=admin123' \
+  -H 'Accept: */*' \
+  -H 'Accept-Encoding: gzip, deflate' \
+  -H 'Authorization: Basic Y2xpZW50OnNlY3JldA==' \
+  -H 'Postman-Token: 99cb100f-10b3-4207-b056-c2361b157a89' \
+  -H 'cache-control: no-cache'
+  
+ Response : 
+ 
+ {
+     "access_token": "9fcde40d-4f56-42ce-abb1-fa160e2b6b2b",
+     "token_type": "bearer",
+     "refresh_token": "0e205630-2486-4cdb-8007-7f11b1f0f900",
+     "expires_in": 3599,
+     "scope": "read write"
+ }
+ 
+ 
+### Get Data From Resource
+
+curl -X GET \
+  http://localhost:8081/apps/details \
+  -H 'Authorization: Bearer 9fcde40d-4f56-42ce-abb1-fa160e2b6b2b' \
+  -H 'Postman-Token: cc77cf22-5087-4afc-9afa-e8ea45cff484' \
+  -H 'cache-control: no-cache'
+  
+  
+  
+  Response : 
+  
+  {
+      "id": "001",
+      "name": "Sample App"
+  }
--- scripts/schema.sql
+++ scripts/schema.sql
@@ -7,4 +7,44 @@ CREATE TABLE IF NOT EXISTS app (
   app_name VARCHAR(50)
 );
 
+drop table if exists oauth_access_token;
+CREATE TABLE IF NOT EXISTS oauth_access_token (
+    token_id VARCHAR(50),
+    token BLOB,
+    authentication_id VARCHAR(50) PRIMARY KEY,
+    user_name VARCHAR(50),
+    client_id VARCHAR(50),
+    authentication BLOB,
+    refresh_token VARCHAR(50)
+);
+
+drop table if exists oauth_refresh_token;
+CREATE TABLE IF NOT EXISTS oauth_refresh_token (
+    token_id VARCHAR(256),
+    token BLOB,
+    authentication BLOB
+);
+
+
+drop table if exists oauth_client_details;
+CREATE TABLE IF NOT EXISTS oauth_client_details (
+    client_id VARCHAR(50) PRIMARY KEY,
+    resource_ids VARCHAR(50),
+    client_secret VARCHAR(100),
+    scope VARCHAR(50),
+    authorized_grant_types VARCHAR(150),
+    web_server_redirect_uri VARCHAR(50),
+    authorities VARCHAR(50),
+    access_token_validity INTEGER,
+    refresh_token_validity INTEGER,
+    additional_information VARCHAR(150),
+    autoapprove VARCHAR(50)
+);
+
+
+INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, web_server_redirect_uri,
+ authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove)
+ VALUES ("client", "$2a$10$xzhBmHVgSFog1FwNbdx4e.INnyNC5yZ9Lc1ZkiF2JsOCSuv8LjaCW", "read,write",
+ "password,authorization_code,refresh_token,client_credentials", null, null, 36000, 36000, null, true);
+
 INSERT INTO app(app_id,username,password,enabled,app_name) values('001','admin','$2a$10$2Qj.gbjlCFezAzkEi6LYnOumhutqMo3h4vPwK4ZWj6kSdV9nemdme',1,'TAP_APP');
